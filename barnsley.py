2771c9ad7e7361dd108ede3c2ba99fd207b7cdf7
pichaelle <pichaelle@pichaelle.local>
2016-07-17 15:58:46 +0900

    てすと

--- barnsley.py
+++ barnsley.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 
+
 import random
 import matplotlib.pyplot as plt
 
@@ -10,3 +11,11 @@ def base_transformation(p, pxx, pxy, pyx, pyy, pyc):
     x1 = pxx*x + pxy*y
     y1 = pyx*x + pyy*y + pyc
     return x1, y1
+
+
+def transformation_(p):
+    base_transformation(p, 0.85, 0.04, -0.04, 0.85)
+
+
+def transformation_2(p):
+    base_transformation(p, 0.2, -0.26, 0.23,)
